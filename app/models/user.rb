a169b72a414dbbd8ce20793bc215d28a70b449bd
ryu57sp <ryuki.baske5@gmail.com>
2021-12-06 07:41:56 +0000

    [Add]フォロー・フォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,25 @@ class User < ApplicationRecord
   has_many :dive_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
   def active_for_authentication?
     super && (self.is_deleted == false)
+  end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 
 end
