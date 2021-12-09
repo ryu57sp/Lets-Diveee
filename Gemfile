dcc2918970321b52dd8bcc01558eeba69fe194bf
ryu57sp <ryuki.baske5@gmail.com>
2021-12-09 08:38:30 +0000

    [Update]いいね機能を非同期通信化

--- Gemfile
+++ Gemfile
@@ -71,8 +71,6 @@ gem 'devise-i18n-views'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 
-gem 'ransack'
-
 gem 'font-awesome-sass', '~> 5.13'
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
@@ -82,3 +80,5 @@ gem 'bootstrap4-kaminari-views'
 
 gem 'pry-byebug'
 gem 'rubocop-airbnb'
+
+gem 'dotenv-rails'
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,14 +7,12 @@ class FavoritesController < ApplicationController
     favorite.save
     #通知機能
     @dive.create_notification_favorite!(current_user)
-    redirect_to dive_path(@dive)
   end
 
   def destroy
     @dive = Dive.find(params[:dive_id])
     favorite = current_user.favorites.find_by(dive_id: @dive.id)
     favorite.destroy
-    redirect_to dive_path(@dive)
   end
 
 end
