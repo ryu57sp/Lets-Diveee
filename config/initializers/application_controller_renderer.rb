c5efa49520b3de6cefe25f04e801affbecd1bb7c
ryu57sp <ryuki.baske5@gmail.com>
2021-12-11 09:41:22 +0000

    [Update]バグを修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8ca8560bf38c9db452bf3eee61bc0e3380ae3f5c289370df8396af587841ffcff5afba2bd666970a67a88f34be6fbb17d0aa83dd103856e4de58b103191b3ccb'
