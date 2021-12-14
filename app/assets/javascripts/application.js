928707f4dbd2ce032fac2e40ae65598bea91b051
ryu57sp <ryuki.baske5@gmail.com>
2021-12-14 16:27:39 +0000

    [Add]トップページにアニメーションを追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,4 +52,23 @@ $(document).on('turbolinks:load', function(){
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
+
+  $(window).on('load scroll', function(){
+    //現時点のスクロールの高さ取得
+    var scrollPosition = $(window).scrollTop();
+    //ウィンドウの高さ取得
+    var windowHeight = $(window).height();
+
+    $('.animation_box').each(function(){
+      //要素の位置（高さ）を取得
+      var elemPosition = $(this).offset().top;
+      //スクロールの高さが要素の位置を超えたら以下のスタイルを適用
+      if(elemPosition < scrollPosition + windowHeight){
+        $(this).css({
+          opacity: 1,
+          transform: 'translateX(0)'
+        });
+      }
+    });
+  });
 });
