0f90c3bf7279e99b10f5020b971fbb3109e2f0dc
ryu57sp <ryuki.baske5@gmail.com>
2021-12-14 10:16:55 +0000

    [Add]ログイン後のテスト

---
+++ spec/system/02_after_login_spec.rb
@@ -0,0 +1,305 @@
+require 'rails_helper'
+
+describe '[STEP2] ユーザログイン後のテスト' do
+  let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:dive) { create(:dive, user: user) }
+  let!(:other_dive) { create(:dive, user: other_user) }
+
+  before do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログイン'
+  end
+
+  describe 'ヘッダーのテスト: ログインしている場合' do
+    context 'リンクの内容を確認: ※ログアウトは『ユーザログアウトのテスト』でテスト済みになります。' do
+      subject { current_path }
+
+      it 'Aboutを押すと、アバウト画面に遷移する' do
+        about_link = find_all('a')[2].native.inner_text
+        about_link = about_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link about_link
+        is_expected.to eq '/about'
+      end
+      it 'マイページを押すと、ユーザー詳細画面に遷移する' do
+        mypage_link = find_all('a')[3].native.inner_text
+        mypage_link = mypage_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link mypage_link, match: :first
+        is_expected.to eq '/users/' + user.id.to_s
+      end
+      it 'ランキングを押すと、いいねランキング画面に遷移する' do
+        ranks_link = find_all('a')[4].native.inner_text
+        ranks_link = ranks_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link ranks_link, match: :first
+        is_expected.to eq '/ranks'
+      end
+      it '新規投稿を押すと、新規投稿画面に遷移する' do
+        new_dive_link = find_all('a')[5].native.inner_text
+        new_dive_link = new_dive_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link new_dive_link, match: :first
+        is_expected.to eq '/dives/new'
+      end
+      it '投稿一覧を押すと、投稿一覧画面に遷移する' do
+        dives_link = find_all('a')[6].native.inner_text
+        dives_link = dives_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link dives_link, match: :first
+        is_expected.to eq '/dives'
+      end
+    end
+  end
+
+  describe '投稿一覧画面のテスト' do
+    before do
+      visit dives_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/dives'
+      end
+      it '自分と他人の画像のリンク先が正しい' do
+        expect(page).to have_link '', href: user_path(dive.user)
+        expect(page).to have_link '', href: user_path(other_dive.user)
+      end
+
+      context '投稿がある場合' do
+        let!(:dive) { create(:dive, user: user) }
+      end
+
+      it '自分の投稿と他人の投稿の投稿日が表示される' do
+        expect(page).to have_content dive.created_at.strftime('%Y/%m/%d')
+        expect(page).to have_content other_dive.created_at.strftime('%Y/%m/%d')
+      end
+      it '投稿者名が表示される' do
+        expect(page).to have_content dive.user.name
+        expect(page).to have_content other_dive.user.name
+      end
+      it '自分の投稿と他人の投稿のタイトルが表示される' do
+        expect(page).to have_content dive.title
+        expect(page).to have_content other_dive.title
+      end
+      it '自分の投稿と他人の投稿のいいね数が表示される' do
+        expect(page).to have_content dive.favorites.count
+        expect(page).to have_content other_dive.favorites.count
+      end
+      it '自分の投稿と他人の投稿のコメント数が表示される' do
+        expect(page).to have_content dive.dive_comments.count
+        expect(page).to have_content other_dive.dive_comments.count
+      end
+    end
+  end
+
+  describe '投稿詳細画面のテスト' do
+    before do
+      visit dive_path(dive.id)
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/dives/' + dive.id.to_s
+      end
+      it '投稿日が表示される' do
+        dive.created_at.strftime('%Y/%m/%d')
+      end
+      it 'ユーザーネームが表示される' do
+        expect(page).to have_content dive.user.name
+      end
+      it 'ポイント名が表示される' do
+        expect(page).to have_content dive.dive_point
+      end
+      it '内容が表示される' do
+        expect(page).to have_content dive.body
+      end
+      it '水温が表示される' do
+        expect(page).to have_content dive.water_temperature
+      end
+      it '潜水深度が表示される' do
+        expect(page).to have_content dive.maximum_depth
+      end
+      it '時期が表示される' do
+        expect(page).to have_content dive.season
+      end
+      it '利用したショップが表示される' do
+        expect(page).to have_content dive.dive_shop
+      end
+      it 'いいね数が表示される' do
+        expect(page).to have_content dive.favorites.count
+      end
+      it 'コメント数が表示される' do
+        expect(page).to have_content dive.dive_comments.count
+      end
+      it 'PV数が表示される' do
+        expect(page).to have_content dive.impressionist_count
+      end
+      it 'いいねボタンのリンクが正しい' do
+        expect(page).not_to have_link '' , href: dive_favorites_path(user)
+      end
+      it 'ブックマークボタンのリンクが正しい' do
+        expect(page).not_to have_link '' , href: dive_bookmarks_path(user)
+      end
+    end
+  end
+
+  describe '新規投稿画面のテスト' do
+    before do
+      visit new_dive_path
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/dives/new'
+      end
+      it '画像投稿フォームがある' do
+        expect(page).to have_field 'dive[image]'
+      end
+      it 'ポイント名投稿フォームがある' do
+        expect(page).to have_field 'dive[dive_point]'
+      end
+      it 'タイトル投稿フォームがある' do
+        expect(page).to have_field 'dive[title]'
+      end
+      it '内容投稿フォームがある' do
+        expect(page).to have_field 'dive[body]'
+      end
+      it '水温投稿フォームがある' do
+        expect(page).to have_field 'dive[water_temperature]'
+      end
+      it '潜水深度投稿フォームがある' do
+        expect(page).to have_field 'dive[maximum_depth]'
+      end
+      it '時期投稿フォームがある' do
+        expect(page).to have_field 'dive[season]'
+      end
+      it '利用したショップ投稿フォームがある' do
+        expect(page).to have_field 'dive[dive_shop]'
+      end
+    end
+  end
+
+   describe '自分の投稿編集画面のテスト' do
+    before do
+      visit edit_dive_path(dive)
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/dives/' + dive.id.to_s + '/edit'
+      end
+      it '「Edit Adventure」と表示される' do
+        expect(page).to have_content 'Edit Adventure🤿'
+      end
+      it 'ポイント名編集フォームが表示される' do
+        expect(page).to have_field 'dive[dive_point]', with: dive.dive_point
+      end
+      it 'タイトル編集フォームが表示される' do
+        expect(page).to have_field 'dive[title]', with: dive.title
+      end
+      it '内容編集フォームが表示される' do
+        expect(page).to have_field 'dive[body]', with: dive.body
+      end
+      it '水温編集フォームが表示される' do
+        expect(page).to have_field 'dive[water_temperature]'
+      end
+      it '潜水深度編集フォームが表示される' do
+        expect(page).to have_field 'dive[maximum_depth]'
+      end
+      it '時期編集フォームが表示される' do
+        expect(page).to have_field 'dive[season]'
+      end
+      it '利用したショップ編集フォームが表示される' do
+        expect(page).to have_field 'dive[dive_shop]', with: dive.dive_shop
+      end
+      it '更新ボタンが表示される' do
+        expect(page).to have_button '更新'
+      end
+    end
+  end
+
+  describe '自分のユーザ詳細画面のテスト' do
+    before do
+      visit user_path(user)
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/' + user.id.to_s
+      end
+      it 'プロフィール画像が表示される' do
+        expect(page).to have_content user.profile_image
+      end
+      it 'ユーザーネームが表示される' do
+        expect(page).to have_content user.name
+      end
+      it '紹介文が表示される' do
+        expect(page).to have_content user.profile
+      end
+      it 'ダイバーズランクが表示される' do
+        expect(page).to have_content user.diver_rank
+      end
+      it '投稿数が表示される' do
+        expect(page).to have_content user.dives.count
+      end
+      it 'フォロー数が表示される' do
+        expect(page).to have_content user.followings.count
+      end
+      it 'フォロワー数が表示される' do
+        expect(page).to have_content user.followers.count
+      end
+      it 'マイページ編集画面へのリンクが正しい' do
+        expect(page).to have_link '編集する', href: edit_user_path(user)
+      end
+      it '自分の各投稿へのリンクが正しい' do
+        expect(page).to have_link dive.image, href: dive_path(dive)
+      end
+      it '他人の投稿は表示されない' do
+        expect(page).not_to have_link '', href: user_path(other_user)
+        expect(page).not_to have_content other_dive
+      end
+    end
+  end
+
+  describe '自分のユーザ情報編集画面のテスト' do
+    before do
+      visit edit_user_path(user)
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/' + user.id.to_s + '/edit'
+      end
+      it '「Edit Profile」と表示される' do
+        expect(page).to have_content 'Edit Profile'
+      end
+      it 'ユーザーネーム編集フォームに自分のユーザーネームが表示される' do
+        expect(page).to have_field 'user[name]', with: user.name
+      end
+      it '紹介文編集フォームに自分の紹介文が表示される' do
+        expect(page).to have_field 'user[profile]'
+      end
+      it 'ダイバーズランク編集フォームに自分のダイバーズランクが表示される' do
+        expect(page).to have_field 'user[diver_rank]'
+      end
+      it '更新ボタンが表示される' do
+        expect(page).to have_button '更新'
+      end
+    end
+  end
+
+  describe '他のユーザ詳細画面のテスト' do
+    before do
+      visit user_path(other_user)
+    end
+
+    context '表示の確認' do
+      it 'フォローボタンのリンクが正しい' do
+        expect(page).not_to have_link 'フォローする' , href: user_relationships_path(user)
+      end
+      it 'DMのリンクが正しい' do
+        expect(page).not_to have_link 'ダイレクトメッセージへ' , href: chat_path(user)
+      end
+    end
+  end
+
+end
