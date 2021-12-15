9b60ca997b0cfe199d788b35f2de9eb5a075e5df
ryu57sp <ryuki.baske5@gmail.com>
2021-12-15 04:43:52 +0000

    [Update]フォロー・フォロワー機能を非同期通信化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,12 +6,11 @@ class RelationshipsController < ApplicationController
     #通知機能
     @user = User.find(params[:user_id])
     @user.create_notification_follow!(current_user)
-    redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
 
   def followings
