ab180190cbda8d342766dbf106396f0dd8232ddb
ryu57sp <ryuki.baske5@gmail.com>
2021-12-15 15:42:56 +0000

    [Update]ランキング一覧のレイアウトを修正

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,5 +1,5 @@
 class RanksController < ApplicationController
   def index
-    @all_ranks = Dive.find(Favorite.group(:dive_id).where(created_at: Time.current.all_day).order('count(dive_id) desc').limit(3).pluck(:dive_id))
+    @all_ranks = Dive.includes(:user).find(Favorite.group(:dive_id).where(created_at: Time.current.all_day).order('count(dive_id) desc').limit(3).pluck(:dive_id))
   end
 end
