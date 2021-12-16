32b33c752bbb83d40acfff0ef1192c8d246ab06c
ryu57sp <ryuki.baske5@gmail.com>
2021-12-16 11:58:27 +0000

    [Add]コメント返信機能＆バグを修正

--- app/models/dive_comment.rb
+++ app/models/dive_comment.rb
@@ -1,6 +1,8 @@
 class DiveComment < ApplicationRecord
   belongs_to :user
   belongs_to :dive
+  belongs_to :dive_comment, foreign_key: :reply, optional: true
+  has_many :replies, class_name: 'DiveComment', foreign_key: :reply, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
   validates :comment, presence: true
