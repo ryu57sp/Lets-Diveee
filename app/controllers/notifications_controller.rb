770c004769b3e98c7fb1ecec352a5910df8d1997
ryu57sp <ryuki.baske5@gmail.com>
2021-12-20 15:17:57 +0000

    [Update]レイアウトを修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.passive_notifications.includes(:visitor).includes(:visited).page(params[:page]).per(20)
+    @notifications = current_user.passive_notifications.includes(:visitor).includes(:visited).page(params[:page]).per(10)
     @notifications.includes(:visitor).includes(:visited).where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
