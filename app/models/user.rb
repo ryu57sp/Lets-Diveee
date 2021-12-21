6c0bf35d0a62e878a3a5e854e7e4ebd81bec18e2
ryu57sp <ryuki.baske5@gmail.com>
2021-12-21 10:57:39 +0000

    [Update]Rubocopでコーディング違反を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[google_oauth2]
-
+         :omniauthable, omniauth_providers: %i(google_oauth2)
 
   attachment :profile_image
 
@@ -61,7 +60,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 end
